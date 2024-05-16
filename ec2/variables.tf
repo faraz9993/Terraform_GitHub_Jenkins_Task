a8b2fccf7c9d5132c777e0e110747c22c78602cb
faraz9993 <147621846+faraz9993@users.noreply.github.com>
2024-05-16 16:26:00 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -2,6 +2,15 @@ variable "ami_id" {
   default = "ami-09040d770ffe2224f"
 }
 
+data "local_file" "public_key" {
+  filename = "ec2/id_rsa.pub"
+}
+
+locals {
+  public_key_content = data.local_file.public_key.content
+}
+
+
 variable "key_name" {
   default     = "Shivi_key"
   description = "The name of the EC2 key pair"
