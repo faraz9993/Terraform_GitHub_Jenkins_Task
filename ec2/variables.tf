eb242d41ea96a6055a75c7baa8fa2f8051b2a762
faraz9993 <147621846+faraz9993@users.noreply.github.com>
2024-05-16 16:12:11 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -8,16 +8,3 @@ variable "key_name" {
   type        = string
 }
 
-data "local_file" "public_key" {
-  filename = "/home/einfochips/.ssh/id_rsa.pub"
-}
-
-locals {
-  public_key_content = data.local_file.public_key.content
-}
-
-
-variable "my_ip" {
-  default = "14.97.73.250"
-  type    = string
-}
